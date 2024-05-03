819b9462644327afa9ca62de932c3fc23e3a2db6
leon <90662048+chinwee123@users.noreply.github.com>
2024-05-03 18:02:44 +0800

    WIP BSTree

--- CSharp/BSTree/BSTree.cs
+++ CSharp/BSTree/BSTree.cs
@@ -1,6 +1,140 @@
-﻿namespace BSTree;
+﻿namespace DSA.BSTrees;
 
-public class Class1
+public class BSTree<T> where T : IComparable<T>
 {
+    public class BSTreeNode<T2> where T2 : IComparable<T2>
+    {
+        public T2              Value;
 
+        public BSTreeNode<T2>? Left;
+        public BSTreeNode<T2>? Right;
+
+        public BSTreeNode(T2 value)
+        {
+            Value = value;
+            Left  = null;
+            Right = null;
+        }
+    }
+
+    public BSTreeNode<T>? Root { get; private set; }
+
+    public int Count { get; private set; }
+
+    public BSTree()
+    {
+        Root  = null;
+        Count = 0;
+    }
+
+    public BSTree(T value)
+    {
+        Root  = new BSTreeNode<T>(value);
+        Count = 1;
+    }
+
+    public void Insert(T value)
+    {
+        Root = Insert(Root, value);
+    }
+
+    private BSTreeNode<T> Insert(BSTreeNode<T>? node, T value)
+    {
+        if (node == null)   // New node
+        {
+            node = new BSTreeNode<T>(value);
+
+            ++Count;
+            return node;
+        }
+
+        int comparer = value.CompareTo(node.Value);
+
+        if (comparer == 0)  // Duplicate values
+        {
+            throw new InvalidOperationException("Duplicate values not allowed.");
+        }
+
+        if (comparer < 0)   // Value smaller than current node
+        {
+            node.Left = Insert(node.Left, value);
+        }
+        else if (comparer > 0)  // Value greater than current node
+        {
+            node.Right = Insert(node.Right, value);
+        }
+
+        return Root!;
+    }
+
+    public void Remove(T value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Clear()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Contains(T value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void PrintInorder(int indent = 0)
+    {
+        if (Root == null)
+            return;
+
+        Inorder(Root, indent);
+    }
+
+    private void Inorder(BSTreeNode<T>? node, int indent = 0)
+    {
+        if (node == null)
+            return;
+
+        Inorder(node!.Left, indent + 1);
+
+        Console.Write(new string(' ', indent * 4));
+        Console.Write(node!.Value);
+
+        Inorder(node!.Right, indent + 1);
+    }
+
+    public void PreorderTraversal(Action<T> action)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void PostorderTraversal(Action<T> action)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int Height()
+    {
+        throw new NotImplementedException();
+    }
+
+    public T Minimum()
+    {
+        throw new NotImplementedException();
+    }
+
+    public T Maximum()
+    {
+        throw new NotImplementedException();
+    }
+
+    public T Predecessor(T value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public T Successor(T value)
+    {
+        throw new NotImplementedException();
+    }
 }
--- CSharp/Tests/BSTree_Tests.cs
+++ CSharp/Tests/BSTree_Tests.cs
@@ -1,6 +1,162 @@
-namespace DSA.BSTrees;
+namespace DSA.BSTrees.Tests;
 
-public class BSTree<T>
+public class BSTrees_Tests
 {
-    
+    [TestFixture]
+    public class BSTreeTests_Constructors
+    {
+        [Test]
+        public void DefaultConstructor_ShouldCreateEmptyTree()
+        {
+            // Arrange
+            var tree = new BSTree<int>();
+
+            // Assert
+            Assert.That(tree.Root, Is.Null);
+            Assert.That(tree.Count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void ValuedConstructor_ShouldCreateTreeWithRootNode()
+        {
+            // Arrange
+            int value = 42;
+
+            // Act
+            var tree = new BSTree<int>(value);
+
+            // Assert
+            Assert.That(tree.Root, Is.Not.Null);
+            Assert.That(tree.Root.Value, Is.EqualTo(value));
+            Assert.That(tree.Count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void ValuedConstructor_ShouldCreateTreeWithRootNode_WhenValueIsComparable()
+        {
+            // Arrange
+            var value = new ComparableClass(42);
+
+            // Act
+            var tree = new BSTree<ComparableClass>(value);
+
+            // Assert
+            Assert.That(tree.Root, Is.Not.Null);
+            Assert.That(tree.Root.Value, Is.EqualTo(value));
+            Assert.That(tree.Count, Is.EqualTo(1));
+        }
+
+        private class ComparableClass : IComparable<ComparableClass>
+        {
+            public int Value { get; set; }
+
+            public ComparableClass(int value)
+            {
+                Value = value;
+            }
+
+            public int CompareTo(ComparableClass? other)
+            {
+                if (other == null)
+                    return 1;
+
+                return Value.CompareTo(other.Value);
+            }
+        }
+    }
+
+    [TestFixture]
+    public class BSTreeTests_Insertion
+    {
+        [Test]
+        public void Insert_ShouldInsertValueIntoEmptyTree()
+        {
+            // Arrange
+            var tree = new BSTree<int>();
+            int value = 42;
+
+            // Act
+            tree.Insert(value);
+
+            // Assert
+            Assert.That(tree.Root, Is.Not.Null);
+            Assert.That(tree.Root.Value, Is.EqualTo(value));
+            Assert.That(tree.Count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Insert_ShouldInsertValueIntoNonEmptyTree()
+        {
+            // Arrange
+            var tree = new BSTree<int>(50);
+            int value1 = 25;
+            int value2 = 75;
+
+            // Act
+            tree.Insert(value1);            tree.Insert(value2);
+
+            // Assert
+            Assert.That(tree.Root!.Value, Is.EqualTo(50));
+            Assert.That(tree.Root.Left!.Value, Is.EqualTo(value1));            
+            Assert.That(tree.Root.Right!.Value, Is.EqualTo(value2));
+            Assert.That(tree.Count, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void Insert_ShouldInsertDuplicateValuesOnlyOnce()
+        {
+            // Arrange
+            var tree = new BSTree<int>();
+            int value = 42;
+
+            // Act
+            tree.Insert(value);
+
+            // Assert
+            Assert.That(tree.Root!.Value, Is.EqualTo(value));
+            Assert.That(tree.Root.Left, Is.Null);
+            Assert.That(tree.Root.Right, Is.Null);
+            Assert.That(tree.Count, Is.EqualTo(1));
+            Assert.Throws<InvalidOperationException>(() => tree.Insert(value));
+        }
+
+        [Test]
+        public void Insert_ShouldInsertComparableValues()
+        {
+            // Arrange
+            var tree = new BSTree<ComparableClass>();
+            var value1 = new ComparableClass(42);
+            var value2 = new ComparableClass(25);
+            var value3 = new ComparableClass(75);
+
+            // Act
+            tree.Insert(value1);
+            tree.Insert(value2);
+            tree.Insert(value3);
+
+            // Assert
+            Assert.That(tree.Root!.Value, Is.EqualTo(value1));
+            Assert.That(tree.Root.Left!.Value, Is.EqualTo(value2));
+            Assert.That(tree.Root.Right!.Value, Is.EqualTo(value3));
+            Assert.That(tree.Count, Is.EqualTo(3));
+        }
+
+        private class ComparableClass : IComparable<ComparableClass>
+        {
+            public int Value { get; set; }
+
+            public ComparableClass(int value)
+            {
+                Value = value;
+            }
+
+            public int CompareTo(ComparableClass? other)
+            {
+                if (other == null)
+                    return 1;
+
+                return Value.CompareTo(other.Value);
+            }
+        }
+    }
 }
